1f787ae0842e7a66a0c1e242c71d58a7cd929e80
isdood <99700376+isdood@users.noreply.github.com>
2024-12-31 21:04:49 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-// src/main.rs
-mod rskernel;
-mod cpuinit;
-
 use std::env;
 use std::io::{self, Read, Write};
 use std::process::{Command, Stdio};
@@ -9,12 +5,6 @@ use std::os::unix::io::AsRawFd;
 use termios::*;
 
 fn main() {
-    // Initialize the CPU
-    cpuinit::init();
-
-    // Initialize the Kernel
-    rskernel::start_kernel();
-
     let stdin = 0; // stdin file descriptor
     let mut termios = Termios::from_fd(stdin).unwrap();
     let original_termios = termios.clone();
